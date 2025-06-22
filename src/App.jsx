452fe78eb32c0d763579ae93f19d334ec0e93b42
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-06-22 16:23:09 +0500

    Practice component creation in React.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Component from './Component.jsx';
+
 function App(){
   return(
+    <div>
 
+    
     <h1>Hello</h1>
+    <Component/>
+    </div>
   )
 }
 
---
+++ src/Component.jsx
@@ -0,0 +1,12 @@
+
+function Component(){
+    return(
+        <div>
+            <h1>
+                Learning Components.
+                </h1>
+                </div>
+    )
+}
+
+export default Component;
