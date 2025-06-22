4a025e16280248ea36ea674e82737243fde40067
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-06-23 00:13:33 +0500

    Learning props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Component from './Component.jsx';
 import State from './State.jsx';
+import User from './User.jsx';
 function App(){
   return(
     <div>
     <h1>Hello</h1>
     <Component/>
     <State />
+    <User name = "Ghania"/>
     </div>
   )
 }
